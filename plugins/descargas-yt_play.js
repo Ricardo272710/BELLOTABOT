b351c58cac97ee79ca80c9ba5f2c35559551dd98
Ricardo272710 <168157929+Ricardo272710@users.noreply.github.com>
2024-11-18 01:32:02 -0500

    Update descargas-yt_play.js

--- plugins/descargas-yt_play.js
+++ plugins/descargas-yt_play.js
@@ -1,44 +1,47 @@
-import fetch from 'node-fetch';
+/*import fetch from 'node-fetch';
 import yts from 'yt-search';
 import ytdl from 'ytdl-core';
 import axios from 'axios';
 import {youtubedl, youtubedlv2} from '@bochilteam/scraper';
 const handler = async (m, {conn, command, args, text, usedPrefix}) => {
-
-if (command == 'play' || command == 'play2') {
-if (!text) return conn.reply(m.chat, `*🤔Que esta buscado? 🤔*\n*Ingrese el nombre del la canción*\n\n*Ejemplo:*\n#play emilia 420`, m, {contextInfo: {externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, title: wm, body: '', previewType: 0, thumbnail: img.getRandom(), sourceUrl: redes.getRandom()}}})
+if (!text) throw `_𝐄𝐬𝐜𝐫𝐢𝐛𝐞 𝐮𝐧𝐚 𝐩𝐞𝐭𝐢𝐜𝐢𝐨́𝐧 𝐥𝐮𝐞𝐠𝐨 𝐝𝐞𝐥 𝐜𝐨𝐦𝐚𝐧𝐝𝐨 𝐞𝐣𝐞𝐦𝐩𝐥𝐨:_ \n*${usedPrefix + command} Billie Eilish - Bellyache*`
+try { 
 const yt_play = await search(args.join(' '))
-const texto1 = `╭───≪~*╌◌ᰱ•••⃙❨͟͞P̸͟͞L̸͟A̸͟͞Y̸͟͞❩⃘•••ᰱ◌╌*~*
-│║📌 *Título* : ${yt_play[0].title}
-│║📆 *Publicado:* ${yt_play[0].ago}
-│║⌚ *Duración:* ${secondString(yt_play[0].duration.seconds)}
-│║👀 *Vistas:* ${MilesNumber(yt_play[0].views)}
-│║
-│║        *████████████┃%100*
-╰─•┈┈┈•••✦𝒟ℳ✦•••┈┈┈•─╯⟤`.trim()
+const texto1 = `
+╭ׅׄ̇─͓̗̗─ׅ̻ׄ╮۪̇߭⊹߭̇︹ׅ̟ׄ̇︹ׅ۪ׄ̇߭︹ׅ̟ׄ̇⊹۪̇߭︹ׅ̟ׄ̇︹ׅ۪ׄ̇߭︹ׅ̟ׄ̇⊹۪̇߭︹ׅ̟ׄ̇︹ׅ۪ׄ̇߭︹ׅ̟ׄ̇߭︹ׅ۪ׄ̇߭̇⊹
+┟─⬪࣪ꥈ𑁍⃪࣭۪ٜ݊݊݊݊݊໑ٜ࣪ᴅᴇsᴄᴀʀɢᴀs໑⃪࣭۪ٜ݊݊݊݊𑁍ꥈ࣪⬪╮
+╭┄─🍂⬪࣪ꥈ𑁍⃪࣭۪ٜ݊݊݊݊݊໑ٜ࣪H̸u̸T̸a̸o̸M̸D̸໑⃪࣭۪ٜ݊݊݊݊𑁍ꥈ࣪⬪╯
+│
+├ ఌ︎♫︎T͜͡i͜͡t͜͡u͜͡l͜͡o͜͡
+├» ${yt_play[0].title}
+├╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌┄
+├ ♫︎♡︎ P̆̈ŭ̈b̆̈l̆̈ĭ̈c̆̈ă̈d̆̈ŏ̈
+├» ${yt_play[0].ago}
+├╌╌╌╌╌╌╌╌╌╌╌╌╌╌┈
+├ ♫︎🝮︎︎︎ᴅᴜʀᴀᴄɪᴏɴ
+├» ${secondString(yt_play[0].duration.seconds)}
+├╌╌╌╌╌╌╌╌╌╌╌╌┄
+├ ❥︎ꨄ︎ 𝐕𝐢𝐬𝐭𝐚𝐬
+├» ${MilesNumber(yt_play[0].views)}
+├╌╌╌╌╌╌╌╌╌╌┄
+├ ✿︎シ︎ 𝔸𝕦𝕥𝕠𝕣
+├» ${yt_play[0].author.name}
+├╌╌╌╌╌╌╌╌┈
+├ ➪♫︎ 𝐸𝑛𝑙𝑎𝑐𝑒
+├» ${yt_play[0].url}
+╰ׁ̻۫─۪۬─۟─۪─۫─۪۬─۟─۪─۟─۪۬─۟─۪─۟─۪۬─۟─۪─۟┄۪۬┄۟┄۪┈۟┈۪`.trim()
 
-await conn.sendButton(m.chat, texto1, botname, yt_play[0].thumbnail, [['Audio', `${usedPrefix}ytmp3 ${yt_play[0].url}`], ['video', `${usedPrefix}ytmp4 ${yt_play[0].url}`]], m)
-}
-
-if (command == 'play3' || command == 'play4') {
-if (!text) return conn.reply(m.chat, `*🤔Que esta buscado? 🤔*\n*Ingrese el nombre del la canción*\n\n*Ejemplo:*\n#play emilia 420`, m, {contextInfo: {externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, title: wm, body: '', previewType: 0, thumbnail: img.getRandom(), sourceUrl: redes.getRandom()}}})
-const yt_play = await search(args.join(' '))
-const texto1 = `╭───≪~*╌◌ᰱ•••⃙❨͟͞P̸͟͞L̸͟A̸͟͞Y̸͟͞❩⃘•••ᰱ◌╌*~*
-│║📌 *Título* : ${yt_play[0].title}
-│║📆 *Publicado:* ${yt_play[0].ago}
-│║⌚ *Duración:* ${secondString(yt_play[0].duration.seconds)}
-│║👀 *Vistas:* ${MilesNumber(yt_play[0].views)}
-│║
-│║        *████████████┃%100*
-╰─•┈┈┈•••✦𝒟ℳ✦•••┈┈┈•─╯⟤`.trim()
-
-await conn.sendButton(m.chat, texto1, botname, yt_play[0].thumbnail, [['Audio', `${usedPrefix}ytmp3 ${yt_play[0].url}`], ['video', `${usedPrefix}ytmp4 ${yt_play[0].url}`], ['Mas resultados', `${usedPrefix}yts ${text}`]], m)
+await conn.sendButton(m.chat, wm, texto1, yt_play[0].thumbnail, [['⍟ 𝐌 𝐏 3 ',`${usedPrefix}supermusic ${yt_play[0].url}`],['𑁍 𝐕 𝐈 𝐃 𝐄 𝐎',`${usedPrefix}supervideo ${yt_play[0].url}`]], null, null, fgif2)
+} catch (e) {
+await conn.reply(m.chat, `*[ ! ] ʜᴜʙᴏ ᴜɴ ᴇʀʀᴏʀ ᴇɴ ᴇʟ ᴄᴏᴍᴀɴᴅᴏ ᴘᴏʀ ғᴀᴠᴏʀ ɪɴᴛᴇɴᴛᴀ ᴍᴀs ᴛᴀʀᴅᴇ..*`, fkontak, m, rcanal)
+console.log(`❗❗ᴇʀʀᴏʀ ${usedPrefix + command} ❗❗`)
+console.log(e)
+handler.limit = 0
 }}
-handler.help = ['play', 'play2'];
-handler.tags = ['downloader'];
 handler.command = ['play', 'play2', 'play3', 'play4']
 //handler.limit = 3
-handler.register = true 
+//handler.register = false 
+handler.group = true
 export default handler;
 
 async function search(query, options = {}) {
@@ -65,4 +68,4 @@ const hDisplay = h > 0 ? h + (h == 1 ? ' hora, ' : ' horas, ') : '';
 const mDisplay = m > 0 ? m + (m == 1 ? ' minuto, ' : ' minutos, ') : '';
 const sDisplay = s > 0 ? s + (s == 1 ? ' segundo' : ' segundos') : '';
 return dDisplay + hDisplay + mDisplay + sDisplay;
-  }
+}*/
