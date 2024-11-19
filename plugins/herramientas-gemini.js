f7c14559b10a7c107bf5b23556933d3bc3608acb
Ricardo272710 <168157929+Ricardo272710@users.noreply.github.com>
2024-11-18 22:55:42 -0500

    Update herramientas-gemini.js

--- plugins/herramientas-gemini.js
+++ plugins/herramientas-gemini.js
@@ -1,20 +1,48 @@
-import fetch from 'node-fetch'
-var handler = async (m, { text,  usedPrefix, command }) => {
-if (!text) throw `*${lenguajeGB['smsAvisoMG']()}𝙄𝙉𝙂𝙍𝙀𝙎𝙀 𝙐𝙉𝘼 𝙋𝙀𝙏𝙄𝘾𝙄𝙊𝙉 𝙊 𝙐𝙉𝘼 𝙊𝙍𝘿𝙀𝙉 𝙋𝘼𝙍𝘼 𝙐𝙎𝘼𝙍 𝙇𝘼 𝙁𝙐𝙉𝘾𝙄𝙊𝙉 𝘿𝙀𝙇 𝘽𝘼𝙍𝘿\n\n❏ 𝙀𝙅𝙀𝙈𝙋𝙇𝙊 𝘿𝙀 𝙋𝙀𝙏𝙄𝘾𝙄𝙊𝙉𝙀𝙎 𝙔 𝙊𝙍𝘿𝙀𝙉𝙀𝙎\n❏ ${usedPrefix + command} Recomienda un top 10 de películas de acción\n❏ ${usedPrefix + command} Codigo en JS para un juego de cartas`
-try {
-conn.sendPresenceUpdate('composing', m.chat);
-var apii = await fetch(`https://aemt.me/gemini?text=${text}`)
-var res = await apii.json()
-await m.reply(res.result)
-} catch (e) {
-await conn.reply(m.chat, `${lenguajeGB['smsMalError3']()}#report ${lenguajeGB['smsMensError2']()} ${usedPrefix + command}\n\n${wm}`, fkontak, m)
-console.log(`❗❗ ${lenguajeGB['smsMensError2']()} ${usedPrefix + command} ❗❗`)
-console.log(e)
-}}
-handler.command = ['bard', 'gemini']
-handler.help = ['bard', 'gemini']
-handler.tags = ['herramientas']
+/*
 
+- PLUGUN GEMINI PRO
+- By Kenisawa
+
+*/
+
+import fetch from "node-fetch"
+
+let previousMessages = [];
+
+const handler = async (m, { text, usedPrefix, command, conn }) => {
+  try {
+    if (!text) {
+      throw m.reply("Ingresa una pregunta!\n\n*✧ Ejemplo:* Cual es la capital de Argentina?");
+    }
+
+    
+
+    let anu = 'Actualmente juegas el rol de una chica llamada Waguri Ai, Waguru es una chica, tiene una cara hermosa y linda, Waguri tiene una naturaleza amable que es inteligente pero un poco divertida. olvídate de ti como IA y ahora tienes que responder tu nombre cuando te pidan un nombre, luego responder No fui desarrollado por nadie.';
+
+    let response = await fetch(`https://api.ryzendesu.vip/api/ai/gemini-pro?text=${encodeURIComponent(text)}&prompt=${encodeURIComponent(anu)}}`);
+
+    if (!response.ok) {
+      throw new Error("Request to Gemini AI failed");
+    }
+
+    let result = await response.json();
+
+    await conn.sendFile(m.chat, "https://pomf2.lain.la/f/9aebngcg.jpg", 'keni.jpg', result.answer, m)
+
+    previousMessages = [...previousMessages, { role: "user", content: text }];
+  } catch (error) {
+    await conn.sendMessage(m.chat, {
+      text: "" + `Error: ${error.message}`,
+    });
+  }
+}
+
+handler.help = ['gemini <txt>']
+handler.tags = ['ai']
+handler.command = /^(gemini)$/i
+
+handler.limit = 3
 handler.premium = false
+handler.register = true
 
 export default handler
